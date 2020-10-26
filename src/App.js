d4f112aef6f0e3e340110fc0739c3f55c7b4cbbc
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-26 10:50:58 -0400

    in Question component, used map() method to iterate the properties from data component; used destructuring in the map() method then structured out layout of the container; in App component, created a useState hook to access the props in the data component for its devault value which can be accessed by the Question component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import data from './data';
 import SingleQuestion from './Question';
 
 const App = () => {
-  return <div>It works?</div>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>Questions and Answers about Login</h3>
+        <SingleQuestion questions={questions} />
+      </section>
+    </main>
+  );
 };
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ questions }) => {
+  return (
+    <>
+      {questions.map((question) => {
+        const { id, title, info } = question;
+        return (
+          <article key={id} className="question">
+            <h4>{title}</h4>
+            <p>{info}</p>
+          </article>
+        );
+      })}
+    </>
+  );
 };
 
 export default Question;
