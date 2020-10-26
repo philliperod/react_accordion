96db8544f2562c20de8424a83843bf2cb24afd28
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-26 11:54:30 -0400

    refactoring by shifting the map() method to the App component and returning the Question component with a key attribute with array.id and spread operator

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ const App = () => {
 
   return (
     <main>
-      <section className="container">
+      <div className="container">
         <h3>Questions and Answers about Login</h3>
-        <SingleQuestion questions={questions} />
-      </section>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
     </main>
   );
 };
 
 export default App;
+
+// made some changes compared to the first commit
+// 1) moved the map() method to App component
+// 2) within the map() method, returned the Question component that has a key attribute of the prop.id value and used the spread operator to expand each prop thats found in the data component
--- src/Question.js
+++ src/Question.js
@@ -1,19 +1,7 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = ({ questions }) => {
-  return (
-    <>
-      {questions.map((question) => {
-        const { id, title, info } = question;
-        return (
-          <article key={id} className="question">
-            <h4>{title}</h4>
-            <p>{info}</p>
-          </article>
-        );
-      })}
-    </>
-  );
+const Question = () => {
+  return <h2>questions component</h2>;
 };
 
 export default Question;
